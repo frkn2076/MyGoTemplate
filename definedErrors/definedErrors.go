51d9af5cd5153a812be0eccb32f28ad660664169
Furkan Öztürk <ozturkfurkan1994@hotmail.com>
2020-10-26 20:51:36 +0300

    e-mail error definitions added

--- definedErrors/definedErrors.go
+++ definedErrors/definedErrors.go
@@ -6,8 +6,10 @@ import(
 
 var UserNotFound error
 var UserAlreadyExists error
+var NotAValidEmail error
 
 func init() {
 	UserNotFound = errors.New("Err-1")
 	UserAlreadyExists = errors.New("Err-2")
+	NotAValidEmail = errors.New("Err-3")
 }
